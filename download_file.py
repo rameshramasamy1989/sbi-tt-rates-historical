b851766d3b139985237a3a6e2f5eaaa5027e47a8
Ramesh Ramasamy <75352331+rameshramasamy1989@users.noreply.github.com>
2025-04-03 21:14:51 +0530

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -3,16 +3,20 @@ import os
 import time
 
 def download_file(url,save_path):
-  response=requests.get(url)
-  response.raise_for_status()
-
+  headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'accept-language': 'en,gu;q=0.9,hi;q=0.8'}
+  sess = requests.Session()
+  response = sess.get(url, headers=headers, timeout=5)
   with open(save_path, 'wb') as file:
     file.write(response.content)
+    
 
 if __name__=="__main__":
   file_url="https://sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf"
 
-save_dir=time.strftime("%Y") + '/' + time.strftime("%m") + '/' + time.strftime("%d")
+save_dir='data'
+#time.strftime("%Y") + '/' + time.strftime("%m") + '/' + time.strftime("%d")
 os.makedirs(save_dir,exist_ok=True)
 timestr = time.strftime("%Y%m%d-%H%M%S")
 save_path=os.path.join(save_dir, "FOREX_CARD_RATES_" + timestr + ".pdf")
